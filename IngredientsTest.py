01cc54dc2cccf9983e1bf2f857f7a299cea77203
Ivan Avdeev <ivpaavdeev@edu.hse.ru>
2026-06-04 02:01:55 +0300

    тесты для задания 1

---
+++ IngredientsTest.py
@@ -0,0 +1,19 @@
+import pytest
+from Ingrediens import Ingredient
+def test_ingredient_creation():
+    ing = Ingredient("Мука", 500.0, "г")
+    assert ing.name == "Мука"
+    assert ing.quantity == 500.0
+    assert ing.unit == "г"
+def test_ingredient_str():
+    ing = Ingredient("Мука", 500.0, "г")
+    assert str(ing) == "Мука: 500.0 г"
+def test_ingredient_eq():
+    ing1 = Ingredient("Мука", 500.0, "г")
+    ing2 = Ingredient("Мука", 300.0, "г")
+    ing3 = Ingredient("Сахар", 500.0, "г")
+    ing4 = Ingredient("Мука", 500.0, "кг")
+    
+    assert ing1 == ing2
+    assert ing1 != ing3
+    assert ing1 != ing4
